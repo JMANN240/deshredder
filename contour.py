4fd57291c5fc8c232b900892d8d9165a97820a76
JT Raber <JMANN240raber@gmail.com>
2023-02-28 14:58:53 -0500

    added some comments on contour

--- contour.py
+++ contour.py
@@ -1,17 +1,8 @@
 import util
 
-def get_adjacent_whites(image, x, y, list, depth=0):
-	current_value = image.getpixel((x,y))
-	if (current_value == 255): # If the current pixel is white
-		list.append((x,y))
-	neighbor_coordinates = ((x-1, y), (x, y-1), (x+1, y), (x, y+1))
-	for neighbor_coordinate in neighbor_coordinates:
-		if neighbor_coordinate not in list and util.coordinate_in_bounds(image, neighbor_coordinate) and image.getpixel(neighbor_coordinate) == 255:
-			get_adjacent_whites(image, neighbor_coordinate[0], neighbor_coordinate[1], list, depth+1)
-	return list
-
 #
 # Represents a closed-loop of white pixels
+# Uses lazy loading
 #
 class Contour:
 	def __init__(self, image):
@@ -20,35 +11,88 @@ class Contour:
 		self._fill = None
 		self._xs = None
 		self._ys = None
+
+	# Recursively find the group of white pixels adjacent to coordinate, return a list of all coordinates
+	def _get_adjacent_whites(self, coordinate, list):
+		list.append(coordinate)
+		(x, y) = coordinate
+
+		# We want to check each neighbor to see if they are white
+		neighbor_coordinates = ((x-1, y), (x, y-1), (x+1, y), (x, y+1))
+		for neighbor_coordinate in neighbor_coordinates:
+			
+			# If we are going to check a neighbor it must satisfy the following
+			new_pixel = neighbor_coordinate not in list # Not already checked
+			pixel_in_image = util.coordinate_in_bounds(self.image, neighbor_coordinate) # Not out of image bounds
+			pixel_is_white = self.image.getpixel(neighbor_coordinate) == 255 # White
+
+			# If it satisfies these conditions, check its neighbors
+			if new_pixel and pixel_in_image and pixel_is_white:
+				self._get_adjacent_whites(self.image, neighbor_coordinate, list)
+
+		return list
 	
+	#
+	# Represents the edge of a contour, returns a list of coordinates
+	#
 	def stroke(self):
+		# Lazy loading
 		if self._stroke is None:
+
+			# Find first white pixel from the left of the image
 			first_coord = util.first_white_from_left(self.image)
+
+			# If we can't find one, there is no contour in the image
 			if first_coord is None:
 				self._stroke = ()
 				return self._stroke
-			(first_x, first_y) = first_coord
-			self._stroke = get_adjacent_whites(self.image, first_x, first_y, [])
+			
+			# Otherwise, get the list of adjacent white coordinates
+			self._stroke = self._get_adjacent_whites(first_coord, [])
 		return self._stroke
-		
+	
+	#
+	# Represents the edge and inner pixels of a contour, returns a list of coordinates
+	#
 	def fill(self):
+		# Lazy loading
 		if self._fill is None:
+
+			# This is where the coordinates will go
 			fill = []
+
+			# For each x-value in the contour
 			for x in self.xs():
+
+				# Get a list of all of the y-values in the column of the x-value
 				xys = [pixel_coordinate[1] for pixel_coordinate in self.stroke() if pixel_coordinate[0] == x]
+
+				# Find the extrema
 				min_y = min(xys)
 				max_y = max(xys)
+
+				# Add all of the points in-between to fill
 				for y in range(min_y, max_y+1):
 					fill.append((x, y))
+			
+			# Turn fill into a tuple for immutability and assign to _fill attribute
 			self._fill = tuple(fill)
 		return self._fill
 
+	#
+	# Returns a sorted list of all x-values in the contour
+	#
 	def xs(self):
+		# Lazy loading
 		if self._xs is None:
 			self._xs = sorted(tuple(set([pixel_coordinate[0] for pixel_coordinate in self.stroke()])))
 		return self._xs
 	
+	#
+	# Returns a sorted list of all y-values in the contour
+	#
 	def ys(self):
+		# Lazy loading
 		if self._ys is None:
 			self._ys = sorted(tuple(set([pixel_coordinate[1] for pixel_coordinate in self.stroke()])))
 		return self._ys
