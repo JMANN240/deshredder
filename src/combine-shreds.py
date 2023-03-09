e0a56c6850e9d7e319333da3602053e2da3b7672
Colby Reinhart <colbyreinhart@gmail.com>
2023-03-09 16:56:26 -0500

    Slight changes to remove-shreds.py

--- src/combine-shreds.py
+++ src/combine-shreds.py
@@ -7,26 +7,11 @@ from PIL import Image
 class Shred:
     def __init__(self, image):
         self.image = image
+
+        # Load a pixel grid to make pixel access more efficient
         pixelGrid = image.load()
 
         # Generate side samples
         self._left_sample = []
         self._right_sample = []
-
-        # For every pixel along the side of the image, move towards the center
-        # until we find a black or white pixel
-        for y in range(image.height):
-            
-            # Left sample
-            x = 0
-            while (pixelGrid[x, y] == None):
-                x += 1
-            _left_sample.append(pixelGrid[x, y])
-
-            # Right sample
-            x = image.width - 1
-            while (pixelGrid[x, y] == None):
-                x -= 1
-            right_sample.append(pixelGrid[x,y])
-
-    #def __convolve_sample(sample):   
+         
